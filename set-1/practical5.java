f9e64b25ea8dba541e3fe473888ebf13e7e71fe9
Radhe Mandani <radhemandani2006@gmail.com>
2026-04-29 13:35:09 +0530

    Create practical5.java

---
+++ set-1/practical5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class practical5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter side b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter side c: ");
+        double c = sc.nextDouble();
+
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+
+            double s = (a + b + c) / 2;
+
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+            System.out.printf("Area of triangle = %.2f", area);
+
+        } else {
+            System.out.println("The given sides cannot form a triangle.");
+        }
+
+        sc.close();
+    }
+}
