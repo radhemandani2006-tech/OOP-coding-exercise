7a970c523489ad3f74eb19e572ac183f9749fcaf
Radhe Mandani <radhemandani2006@gmail.com>
2026-04-29 13:28:22 +0530

    Update practical1.java

--- set-1/practical1.java
+++ set-1/practical1.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class practical1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+
+        double feet = meters * 3.28084;
+
+        System.out.printf("Distance in feet: %.2f", feet);
+
+        sc.close();
+    }
+}
